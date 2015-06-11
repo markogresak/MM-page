90433a71b47f3727a0656265be9c56ed5fc7a583
Marko Grešak <marko@gresak.io>
2015-06-11 02:46:35 +0200

    add watch module and watch images folder for changes

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 'use strict';
 var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var watch = require('watch');
 var port = 9500;
+// Path to images folder.
+var publicPath = path.relative(__dirname, 'public/');
+var imagesPublicPath = 'images';
+var imagesPath = path.resolve(publicPath, imagesPublicPath);
+// Object storing images file names.
+var imagesObj = {
+  '2d': [],
+  '3d': []
+};
+// Watch imagesPath folder, update files on change.
+//  Function is called when watchTree is first declared and on each change inside imagesPath folder.
+watch.watchTree(imagesPath, function () {
+  Object.keys(imagesObj).forEach(function (folder) {
+    fs.readdir(path.resolve(imagesPath, folder), function (err, filesArray) {
+      if (err) {
+        return;
+      }
+      imagesObj[folder] = filesArray.map(function (imageFileName) {
+        return path.relative(__dirname, path.resolve(imagesPublicPath, folder, imageFileName));
+      });
+    });
+  });
+});
+// Init app and serve static files from public, jquery and bootstrap folers.
 var app = express();
 app.use(express.static('public'));
 app.use('/jquery', express.static('node_modules/jquery/dist'));
