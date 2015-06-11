9e386d126414fcda6c632af558e25da6c2e97fc0
Marko Grešak <marko@gresak.io>
2015-06-11 06:46:36 +0200

    exec minify js/css from minify.js script

--- minify.js
+++ minify.js
@@ -1,10 +1,22 @@
 'use strict';
 var Imagemin = require('imagemin');
+var exec = require('child_process').exec;
+
+// Minify main.js.
+exec('./node_modules/.bin/uglifyjs --screw-ie8 -m -c --unsafe -o public/main.min.js public/main.js', function () {
+  console.log('minified main.js');
+});
+// Add vendor prefixes to and minify main.css.
+exec('./node_modules/.bin/cleancss -o public/main.min.css public/main.css', function () {
+  console.log('minified main.css');
+});
 
 new Imagemin()
   .src('public/images/**/*.png')
   .dest('public/images')
-  .use(Imagemin.optipng({optimizationLevel: 3}))
-  .run(function (err, files) {
-    // => {path: 'build/images/foo.jpg', contents: <Buffer 89 50 4e ...>}
+  .use(Imagemin.optipng({
+    optimizationLevel: 7
+  }))
+  .run(function () {
+    console.log('minified images');
   });
