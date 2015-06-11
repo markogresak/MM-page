a9a2590ec611073bd77d29ea2b0e82636952c4ec
Marko Grešak <marko@gresak.io>
2015-06-11 07:30:37 +0200

    minify main.js and load-css.js into single file

--- minify.js
+++ minify.js
@@ -3,7 +3,7 @@ var Imagemin = require('imagemin');
 var exec = require('child_process').exec;
 var bin = './node_modules/.bin/';
 // Minify main.js.
-exec(bin + 'uglifyjs --screw-ie8 -m -c --unsafe -o public/main.min.js public/main.js', function () {
+exec(bin + 'uglifyjs --screw-ie8 -m -c --unsafe -o public/main.min.js public/main.js public/load-css.js', function () {
   console.log('minified main.js');
 });
 // Add vendor prefixes to and minify main.css.
